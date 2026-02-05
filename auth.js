d4a414c139a342530ec85a3130730dd2b27f8dfc
kashyapvekariya-growexxer <kashyap.vekariya@growexx.com>
2026-02-05 15:48:37 +0530

    done step 4
    oops forgot validation
    
    typo fix

--- auth.js
+++ auth.js
@@ -1,3 +1,11 @@
+function login(username, password) {
+// Validate credential
+}
+
+function logout() {
+console.log("Logging out");
+}
+
 function validatePassword(password) {
 return password.length >= 8;
 }
